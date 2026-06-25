fce42f7af15619fa96404343822a8163e729318c
GABRIEL <GABRIEL@DESKTOP-5OOOK75>
2026-06-24 22:26:33 -0300

    First commit

---
+++ Core/BaseSystem.cs
@@ -0,0 +1,17 @@
+﻿namespace LosSantosExpanded
+{
+    public abstract class BaseSystem : ISystem
+    {
+        public virtual void Initialize()
+        {
+        }
+
+        public virtual void Update()
+        {
+        }
+
+        public virtual void Shutdown()
+        {
+        }
+    }
+}
---
+++ EntryPoint.cs
@@ -0,0 +1,21 @@
+﻿
+using Rage;
+using System;
+
+[assembly: Rage.Attributes.Plugin("{[LSE] Alpha Teste build v0.001}", Description = "This is a alpha test in currently in development.", Author = "Geekarcanjo")]
+
+namespace LosSantosExpanded
+{
+    public static class EntryPoint
+    {
+        public static void Main()
+        {
+            Game.LogTrivial("[LSE] Starting...");
+
+            ModController.Instance.Initialize();
+            ModController.Instance.Run();
+
+            Game.LogTrivial("[LSE] Shutdown.");
+        }
+    }
+}
